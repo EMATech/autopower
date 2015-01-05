71f84cc4344264eacb7e8df251e0c009206f1157
Raphaël Doursenaud <rdoursenaud@free.fr>
2015-01-05 08:30:15 +0100

    Implemented Telnet client to Denon amplifier
    Switched to Python 2 because twisted.conch is not available in Python 3

--- autopower.py
+++ autopower.py
@@ -1,11 +1,34 @@
-#!/bin/env python
+#!/bin/env python2
 
 """
 Automate powerered loudspeakers power with EATON UPS from Denon DN-500AV pre-amplifier state
 """
 
+from twisted.internet import reactor
+from twisted.internet.protocol import ClientFactory
+from twisted.conch.telnet import TelnetTransport, TelnetProtocol  # Unavailable in Python3, yet
 
-def reactor():
+
+class TelnetPrinter(TelnetProtocol):
+    def connectionMade(self):
+        self.transport.write("PW?\n")
+
+    def dataReceived(self, bytes):
+        print('Received:', repr(bytes))
+        if bytes == 'PWON':
+            print('Power is ON')
+            # TODO: Enable UPS sockets
+        if bytes == 'PWSTANDBY':
+            print('Power is STANDBY')
+            # TODO: Disable UPS sockets
+
+
+class TelnetFactory(ClientFactory):
+    def buildProtocol(self, addr):
+        return TelnetTransport(TelnetPrinter)
+
+
+if __name__ == '__main__':
     """
     The amplifier uses a Telnet interface (port 23) to send and receive serial commands
 
@@ -19,3 +42,7 @@ def reactor():
 
     We want them to follow the amp's state
     """
+    host = '192.168.1.10'  # TODO: store in a configuration file
+    port = 23
+    reactor.connectTCP(host, port, TelnetFactory())
+    reactor.run()
